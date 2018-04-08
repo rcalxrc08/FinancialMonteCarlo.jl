238381bc628d0af4e9e60526bcd7838094d67773
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2018-04-08 18:17:37 +0200

    runner dual

--- src/payoffs/forward.jl
+++ src/payoffs/forward.jl
@@ -7,9 +7,8 @@ end
 export Forward,ForwardData;
 
 function payoff(S::Matrix{num},optionData::ForwardData,Payoff::Forward,isCall::Bool=true) where{num<:Number}
-	ST=S[1:end,end];
-	f(ST::num)::num=ST;
-	payoff2=f.(ST);
 	
-	return payoff2;
+	ST=S[1:end,end];
+	
+	return ST;
 end
