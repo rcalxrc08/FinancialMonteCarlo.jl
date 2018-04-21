e1262d3087c0a472467603f9a723791f8c16a124
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2018-04-21 16:02:09 +0200

    bug dict

--- test/test_black.jl
+++ test/test_black.jl
@@ -11,7 +11,7 @@ Nsim=10000;
 Nstep=30;
 sigma=0.2;
 ParamDict=Dict{String,Number}("sigma"=>sigma)
-mc=MonteCarloBaseData(dict,Nsim,Nstep);
+mc=MonteCarloBaseData(ParamDict,Nsim,Nstep);
 toll=1e-3;
 
 spotData1=equitySpotData(S0,r,d);
