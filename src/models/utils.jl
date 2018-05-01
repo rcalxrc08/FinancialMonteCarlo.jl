07929254314ca536941e02b715e89622f0d12ca0
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2018-05-01 17:41:35 +0200

    ref

--- src/models/utils.jl
+++ src/models/utils.jl
@@ -2,7 +2,7 @@ quantile_exp(lam::Number,rand1::Number)::Number=-log(1-rand1)/lam;
 
 import Distributions.quantile
 
-function quantile(d::Distribution{Distributions.Univariate,Distributions.Continuous},perc::Dual{num}) where {num<:Number}
+function quantile(d::Distribution{Distributions.Univariate,Distributions.Continuous},perc::Number)
 	percval=value(perc);
 	yval=quantile(d,percval);
 	der1=1.0/(pdf(d,yval));
