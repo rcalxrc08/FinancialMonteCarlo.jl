0d06c21c4fb5a7da63334551f69f3c382d1b06ff
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2018-05-19 17:11:46 +0200

    first

--- src/payoffs/forward.jl
+++ src/payoffs/forward.jl
@@ -6,10 +6,14 @@ end
 
 export Forward,ForwardData;
 
-function payoff(S::Matrix{num},optionData::ForwardData,spotData::equitySpotData,Payoff::Forward,isCall::Bool=true) where{num<:Number}
+function payoff(S::Matrix{num},optionData::ForwardData,spotData::equitySpotData,Payoff::Forward,isCall::Bool=true,T1::Float64=T) where{num<:Number}
 	r=spotData.r;
 	T=optionData.T;
-	ST=S[1:end,end];
+	NStep=length(S[1,1:end])-1
+	dt=T1/NStep;
+	index1=round(Int,T/dt);
+	index1=index1>NStep?Nstep+1:index+1;
+	ST=S[1:end,index1+1];
 	
 	return ST*exp(-r*T);
 end
--- src/pricer.jl
+++ src/pricer.jl
@@ -24,3 +24,15 @@ function pricer(mcProcess::MonteCarloProblem,spotData::equitySpotData,mcBaseData
 	return Price;
 end
 
+
+function pricer(mcProcess::MonteCarloProblem,spotData::equitySpotData,mcBaseData::MonteCarloBaseData,optionDatas::Array{OptionData},payoffs1::Array{PayoffMC},isCall::Array{Bool},mode1::MonteCarloMode=standard)
+	srand(0)
+	T=optionDatas.T;
+	r=spotData.r;
+	Nsim=mcBaseData.Nsim;
+	S=simulate(mcProcess,spotData,mcBaseData,T)
+	Payoff=payoff(S,optionDatas,spotData,payoffs1,isCall);
+	Price=mean(Payoff);
+	return Price;
+end
+
