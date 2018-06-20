99c4f80824ade72c8a2c7fe969f360055076567e
Nicola <nicola.scaramuzzino@yandex.com>
2018-06-20 12:12:00 +0200

    amend

--- test/test_parameters.jl
+++ test/test_parameters.jl
@@ -15,21 +15,16 @@ sigma=0.2;
 theta1=0.01;
 k1=0.03;
 sigma1=0.02;
-ParamDict=Dict{String,Number}("sigma"=>sigma)
-ParamDictLonger=Dict{String,Number}("sigma"=>sigma, "theta" => theta1, "k" => k1)
+ParamDictLongest=Dict{String,Number}("sigma"=>sigma, "theta" => theta1, "k" => k1,"theta1" => theta1, "k1" => k1,"theta11" => theta1, "k11" => k1,"theta12" => theta1, "k12" => k1, "k123" => k1, "k12k" => k1)
 ParamDictNeg=Dict{String,Number}("sigma"=>-sigma)
 McNeg=MonteCarloBaseData(ParamDictNeg,Nsim,Nstep);
-McLonger=MonteCarloBaseData(ParamDictLonger,Nsim,Nstep);
-Mc=MonteCarloBaseData(ParamDict,Nsim,Nstep);
+Mc=MonteCarloBaseData(ParamDictLongest,Nsim,Nstep);
 toll=0.8;
 
 spotData1=equitySpotData(S0,r,d);
 
-@test_throws(ErrorException,simulate(BlackScholesProcess(),spotData1,McLonger,T));
 @test_throws(ErrorException,simulate(BlackScholesProcess(),spotData1,McNeg,T));
 @test_throws(ErrorException,simulate(BlackScholesProcess(),spotData1,Mc,-T));
 for type1 in concrete_types(AbstractMonteCarloProcess)
-	if type1()!=BlackScholesProcess()&&type1()!=SubordinatedBrownianMotion()
-		@test_throws(ErrorException,simulate(type1(),spotData1,Mc,T));
-	end
+	@test_throws(ErrorException,simulate(type1(),spotData1,Mc,T));
 end
