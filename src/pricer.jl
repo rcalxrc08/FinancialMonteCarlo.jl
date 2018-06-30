2094a248ecfbaf906808da7a264b42c8560726f1
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2018-06-30 19:19:49 +0200

    name refactor

--- src/pricer.jl
+++ src/pricer.jl
@@ -2,22 +2,22 @@ include("utils.jl")
 include("payoff.jl")
 include("models.jl")
 	
-function pricer(mcProcess::BaseProcess,spotData::equitySpotData,mcBaseData::MonteCarloConfiguration,optionData::AbstractPayoff,mode1::MonteCarloMode=standard)
+function pricer(mcProcess::BaseProcess,spotData::equitySpotData,mcConfig::MonteCarloConfiguration,abstractPayoff::AbstractPayoff,mode1::MonteCarloMode=standard)
 	srand(0)
-	T=optionData.T;
-	Nsim=mcBaseData.Nsim;
-	S=simulate(mcProcess,spotData,mcBaseData,T,mode1)
-	Payoff=payoff(S,optionData,spotData);
+	T=abstractPayoff.T;
+	Nsim=mcConfig.Nsim;
+	S=simulate(mcProcess,spotData,mcConfig,T,mode1)
+	Payoff=payoff(S,abstractPayoff,spotData);
 	Price=mean(Payoff);
 	return Price;
 end
 
-function pricer(mcProcess::BaseProcess,spotData::equitySpotData,mcBaseData::MonteCarloConfiguration,optionDatas::Array{AbstractPayoff},mode1::MonteCarloMode=standard)
+function pricer(mcProcess::BaseProcess,spotData::equitySpotData,mcConfig::MonteCarloConfiguration,abstractPayoffs::Array{AbstractPayoff},mode1::MonteCarloMode=standard)
 	srand(0)
-	maxT=maximum([optionData.T for optionData in optionDatas])
-	Nsim=mcBaseData.Nsim;
-	S=simulate(mcProcess,spotData,mcBaseData,maxT,mode1)
-	Prices=[mean(payoff(S,optionData,spotData,maxT)) for optionData in optionDatas  ]
+	maxT=maximum([abstractPayoff.T for abstractPayoff in abstractPayoffs])
+	Nsim=mcConfig.Nsim;
+	S=simulate(mcProcess,spotData,mcConfig,maxT,mode1)
+	Prices=[mean(payoff(S,abstractPayoff,spotData,maxT)) for abstractPayoff in abstractPayoffs  ]
 	
 	return Prices;
 end
