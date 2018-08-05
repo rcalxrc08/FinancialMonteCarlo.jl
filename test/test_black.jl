545276d90bae37af34770dacdbaaecc70a2119d0
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2018-08-05 15:53:02 +0200

    100%

--- test/test_black.jl
+++ test/test_black.jl
@@ -77,11 +77,18 @@ mc2=MonteCarloConfiguration(Nsim+1,Nstep);
 @test_throws(ErrorException,pricer(Model,spotData1,mc2,AsianFixedStrikeData,MonteCarlo.antithetic));
 
 @test variance(Model,spotData1,mc,EUDataPut)>variance(Model,spotData1,mc,EUDataPut,MonteCarlo.antithetic);
+@test prod(variance(Model,spotData1,mc,[EUDataPut,AMDataPut]).>=variance(Model,spotData1,mc,[EUDataPut,AMDataPut],MonteCarlo.antithetic));
 
 IC1=confinter(Model,spotData1,mc,EUDataPut);
 IC2=confinter(Model,spotData1,mc,EUDataPut,MonteCarlo.antithetic);
 L1=IC1[2]-IC1[1]
 L2=IC2[2]-IC2[1]
+@test L1>L2
+
+IC1=confinter(Model,spotData1,mc,[EUDataPut,AMDataPut]);
+IC2=confinter(Model,spotData1,mc,[EUDataPut,AMDataPut],MonteCarlo.antithetic);
+L1=IC1[2][2]-IC1[2][1]
+L2=IC2[2][2]-IC2[2][1]
 @test L1>L2
 ############## Complete Options
 
