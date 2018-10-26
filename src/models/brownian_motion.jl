61dacc4c4c3dce22cc6770bddbc187d5fa64b925
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2018-10-27 00:19:09 +0200

    dual numbers support

--- src/models/brownian_motion.jl
+++ src/models/brownian_motion.jl
@@ -13,6 +13,8 @@ end
 
 export BrownianMotion;
 
+Float32(p::Dual{Float64})=dual(Float32(p.value),Float32(p.epsilon))
+
 function simulate(mcProcess::BrownianMotion,spotData::equitySpotData,mcBaseData::MonteCarloConfiguration,T::numb,monteCarloMode::MonteCarloMode=standard) where {numb<:Number}
 	Nsim=mcBaseData.Nsim;
 	Nstep=mcBaseData.Nstep;
