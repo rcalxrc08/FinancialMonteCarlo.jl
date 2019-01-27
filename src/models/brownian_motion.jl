076504c4866364e0035370e576deeb2a9ae2a3c3
Unknown <nicola.scaramuzzino@yandex.com>
2019-01-27 21:04:09 +0100

    trial

--- src/models/brownian_motion.jl
+++ src/models/brownian_motion.jl
@@ -52,6 +52,16 @@ function simulate(mcProcess::BrownianMotion,spotData::equitySpotData,mcBaseData:
 		end
 		X=Matrix(X_cu);
 		return X;
+	elseif monteCarloMode==parallel_cuda_gpu_trial
+		mean_bm_f=Float32(mean_bm);
+		stddev_bm_f=Float32(stddev_bm);
+		isDualZero=mean_bm_f*stddev_bm_f*zero(Float32);
+		X_cu=CuMatrix{typeof(isDualZero)}(Nsim,Nstep+1);
+		for i=1:Nstep
+			X_cu[:,i+1]=X_cu[:,i]+(mean_bm_f.+stddev_bm_f.*cu(randn(Float32,Nsim)));
+		end
+		X=Matrix(X_cu);
+		return X;
 	else
 		isDualZero=mean_bm*stddev_bm*0.0;
 		X=Matrix{typeof(isDualZero)}(undef,Nsim,Nstep+1);
