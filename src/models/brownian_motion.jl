229c56242e6ab6fc553aebc5b4742f37ccef9caa
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-03-24 15:35:59 +0100

    bug

--- src/models/brownian_motion.jl
+++ src/models/brownian_motion.jl
@@ -46,7 +46,7 @@ function simulate(mcProcess::BrownianMotion,spotData::equitySpotData,mcBaseData:
 		mean_bm_f=Float32(mean_bm);
 		stddev_bm_f=Float32(stddev_bm);
 		isDualZero=mean_bm_f*stddev_bm_f*zero(Float32);
-		X_cu=CuMatrix{typeof(isDualZero)}(Nsim,Nstep+1);
+		X_cu=CuMatrix{typeof(isDualZero)}(undef,Nsim,Nstep+1);
 		for i=1:Nstep
 			X_cu[:,i+1]=X_cu[:,i]+(mean_bm_f.+stddev_bm_f.*cu(randn(Float32,Nsim)));
 		end
