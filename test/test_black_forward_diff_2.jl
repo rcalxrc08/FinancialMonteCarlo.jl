019c1df47bf9ac9fb66fc2b0925beb3848f7002f
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-04-07 15:31:25 +0200

    new tests for ad

---
+++ test/test_black_forward_diff_2.jl
@@ -0,0 +1,33 @@
+using FinancialMonteCarlo,ForwardDiff;
+@show "Black Scholes Model"
+S0=100.0;
+K=100.0;
+r=0.02;
+T=1.0;
+d=0.01;
+D=90.0;
+
+Nsim=10000;
+Nstep=30;
+sigma=0.2
+mc=MonteCarloConfiguration(Nsim,Nstep);
+toll=0.8
+
+spotData1=equitySpotData(S0,r,d);
+
+FwdData=Forward(T)
+EUData=EuropeanOption(T,K)
+AMData=AmericanOption(T,K)
+BarrierData=BarrierOptionDownOut(T,K,D)
+AsianFloatingStrikeData=AsianFloatingStrikeOption(T)
+AsianFixedStrikeData=AsianFixedStrikeOption(T,K)
+Model=BlackScholesProcess(sigma);
+
+
+f(x::Vector) = pricer(BlackScholesProcess(x[1]),equitySpotData(x[2],r,d),mc,EUData);
+
+x=Float64[sigma,S0]
+
+g = x -> ForwardDiff.gradient(f, x);
+
+@show g(x)
