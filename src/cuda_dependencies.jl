93b02b6c9200cb843e88f7bc2bb50201ef5a3aa7
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-04-08 21:51:05 +0200

    cujl for coverage

--- src/cuda_dependencies.jl
+++ src/cuda_dependencies.jl
@@ -1,6 +1,6 @@
 using .CuArrays
-include("models/cuda/brownian_motion_cuda.jl")
-include("models/cuda/brownian_motion_cuda_2.jl")
-include("models/cuda/heston_cuda.jl")
-include("models/cuda/heston_cuda_2.jl")
-include("models/cuda/subordinated_brownian_motion_cuda.jl")
+include("models/cuda/brownian_motion_cuda.cujl")
+include("models/cuda/brownian_motion_cuda_2.cujl")
+include("models/cuda/heston_cuda.cujl")
+include("models/cuda/heston_cuda_2.cujl")
+include("models/cuda/subordinated_brownian_motion_cuda.cujl")
