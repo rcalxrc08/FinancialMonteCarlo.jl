56b8ea2de7f626fc1b24f16c06f4110bf858db82
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-05-02 01:23:01 +0200

    amend

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,7 @@ using FinancialMonteCarlo
 path1=joinpath(dirname(pathof(FinancialMonteCarlo)),"..","test")
 test_listTmp=readdir(path1);
 BlackList=["REQUIRE","runtests.jl","runner.jl","test_black_cuda.jl","cuda"]
-if(!DIFFEQ_MONTECARLO_ACTIVE_FLAG)
-	BlackList=vcat("test_diffeq_log.jl","test_diffeq.jl","test_diff_eq_montecarlo_array.jl",BlackList)
-end
+BlackList=vcat("test_diffeq_log.jl","test_diffeq.jl","test_diff_eq_montecarlo_array.jl",BlackList)
 test_list=[test_element for test_element in test_listTmp if !Bool(sum(test_element.==BlackList))]
 println("Running tests:\n")
 for (current_test,i) in zip(test_list,1:length(test_list))
