4065f97a686d2fbb8b1adc50fafb77bb7133cf55
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-05-01 13:41:04 +0200

    support for JUMP DIffusion SDE

--- src/models/diff_eq_monte_carlo.jl
+++ src/models/diff_eq_monte_carlo.jl
@@ -5,19 +5,31 @@ function simulate(mcProcess::MonteCarloProblem,spotData::equitySpotData,mcBaseDa
 	Nsim=mcBaseData.Nsim;
 	Nstep=mcBaseData.Nstep;
 	Dt=T/Nstep
-	
+	tt=collect(0.0:Dt:T)
 	if T<=0.0
 		error("Final time must be positive");
 	end
 	sol = solve(mcProcess,SOSRI(),num_monte=Nsim,parallel_type=:threads,dt=Dt,adaptive=false)
 	X0=sol.u[1].u[1];
-	if isapprox(X0,0.0)
-		S=[S0*exp.(path.u[j]) for path in sol.u, j in 1:Nstep];
-		
-		return S;
+	if(!(typeof(mcProcess.prob)<:JumpProblem))
+		if isapprox(X0,0.0)
+			S=[S0*exp.(path.u[j]) for path in sol.u, j in 1:(Nstep+1)];
+			
+			return S;
+		else
+			S=[path.u[j] for path in sol.u, j in 1:(Nstep+1)];
+			
+			return S;
+		end
 	else
-		S=[path.u[j] for path in sol.u, j in 1:Nstep];
-		
-		return S;
+		if isapprox(X0,0.0)
+			S=[S0*exp.(path(tt[j])) for path in sol.u, j in 1:(Nstep+1)];
+			
+			return S;
+		else
+			S=[path(tt[j]) for path in sol.u, j in 1:(Nstep+1)];
+			
+			return S;
+		end
 	end
 end
