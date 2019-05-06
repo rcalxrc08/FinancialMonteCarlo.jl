3bb9bdbaf84fefda0783e1520c3082b4ec9bed20
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-05-06 19:13:49 +0200

    bench

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,16 +1,14 @@
 path1=joinpath(Pkg.dir("FinancialMonteCarlo"),"benchmark")
 test_listTmp=readdir(path1);
-BlackList=["REQUIRE","benchmarks.jl","runner.jl","cuda","af"]
+BlackList=["benchmarks.jl","runner.jl","cuda","af"]
 test_list=[test_element for test_element in test_listTmp if !Bool(sum(test_element.==BlackList))]
 println("Running tests:\n")
-i=1;
-for current_test in test_list
+for (current_test,i) in zip(test_list,1:length(test_list))
 	println("------------------------------------------------------------")
     println("  * $(current_test) *")
-    include(current_test)
+    include(joinpath(path1,current_test))
 	println("------------------------------------------------------------")
 	if (i<length(test_list))
 		println("")
 	end
-	i+=1;
 end
