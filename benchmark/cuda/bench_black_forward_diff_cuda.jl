665fa2444d32071af2ba7aa52d7f485e6320ebe8
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-05-26 00:17:23 +0200

    bench adjusted

--- benchmark/cuda/bench_black_forward_diff_cuda.jl
+++ benchmark/cuda/bench_black_forward_diff_cuda.jl
@@ -32,6 +32,6 @@ y0=g(x);
 
 f_(x::Vector) = pricer(BlackScholesProcess(x[1]),equitySpotData(x[2],x[3],x[4]),mc,AmericanOption(x[5],K),FinancialMonteCarlo.standard,FinancialMonteCarlo.CudaMode_2());
 g_ = x -> ForwardDiff.gradient(f_, x);
-y0=g(x);
+y0=g_(x);
 @btime f_(x);
 @btime g_(x);
