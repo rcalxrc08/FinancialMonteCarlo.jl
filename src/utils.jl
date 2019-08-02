b65871b7ee086c8f57c707811a9c3455ab555e9b
Unknown <nicola.scaramuzzino@yandex.com>
2019-08-02 20:45:05 +0200

    abstractmethod

--- src/utils.jl
+++ src/utils.jl
@@ -17,7 +17,8 @@ abstract type BaseMode end
 struct SerialMode <: BaseMode end
 abstract type ParallelMode <: BaseMode end
 
-abstract type AbstractMonteCarloMethod end
+abstract type AbstractMethod end
+abstract type AbstractMonteCarloMethod <: AbstractMethod end
 struct StandardMC <: AbstractMonteCarloMethod end
 struct AntitheticMC <: AbstractMonteCarloMethod end
 
