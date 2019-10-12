9ceb8fc7b8ddda9b3a5ec1dddda3d14f9fea4de1
Unknown <nicola.scaramuzzino@yandex.com>
2019-10-12 14:22:08 +0200

    pedix kou

--- src/models/kou.jl
+++ src/models/kou.jl
@@ -1,34 +1,34 @@
 """
 Struct for Kou Process
 
-		kouProcess=KouProcess(σ::num1,λ::num2,p::num3,λp::num4,λm::num5) where {num1,num2,num3,num4,num5 <: Number}
+		kouProcess=KouProcess(σ::num1,λ::num2,p::num3,λ₊::num4,λ₋::num5) where {num1,num2,num3,num4,num5 <: Number}
 	
 Where:\n
 		σ  =	volatility of the process.
 		λ  = 	jumps intensity.
 		p  =	prob. of having positive jump.
-		λp =	positive jump size.
-		λm =	negative jump size.
+		λ₊ =	positive jump size.
+		λ₋ =	negative jump size.
 """
 mutable struct KouProcess{num <: Number, num1 <: Number,num2 <: Number,num3 <: Number,num4 <: Number}<:FiniteActivityProcess
 	σ::num
 	λ::num1
 	p::num2
-	λp::num3
-	λm::num4
-	function KouProcess(σ::num,λ::num1,p::num2,λp::num3,λm::num4) where {num <: Number, num1 <: Number,num2 <: Number,num3 <: Number,num4 <: Number}
+	λ₊::num3
+	λ₋::num4
+	function KouProcess(σ::num,λ::num1,p::num2,λ₊::num3,λ₋::num4) where {num <: Number, num1 <: Number,num2 <: Number,num3 <: Number,num4 <: Number}
         if σ<=0.0
 			error("volatility must be positive");
 		elseif λ<=0.0
 			error("jump intensity must be positive");
-		elseif λp<=0.0
+		elseif λ₊<=0.0
 			error("positive λ must be positive");
-		elseif λm<=0.0
+		elseif λ₋<=0.0
 			error("negative λ must be positive");
 		elseif !(0<=p<=1)
 			error("p must be a probability")
         else
-            return new{num,num1,num2,num3,num4}(σ,λ,p,λp,λm)
+            return new{num,num1,num2,num3,num4}(σ,λ,p,λ₊,λ₋)
         end
     end
 end
@@ -44,8 +44,8 @@ function simulate(mcProcess::KouProcess,spotData::equitySpotData,mcBaseData::Mon
 	σ=mcProcess.σ;
 	λ1=mcProcess.λ;
 	p=mcProcess.p;
-	λp=mcProcess.λp;
-	λm=mcProcess.λm;
+	λ₊=mcProcess.λ₊;
+	λ₋=mcProcess.λ₋;
 	if T<=0.0
 		error("Final time must be positive");
 	end
@@ -53,7 +53,7 @@ function simulate(mcProcess::KouProcess,spotData::equitySpotData,mcBaseData::Mon
 	####Simulation
 	## Simulate
 	# r-d-psi(-i)
-	drift_RN=r-d-σ^2/2-λ1*(p/(λp-1)-(1-p)/(λm+1));
+	drift_RN=r-d-σ^2/2-λ1*(p/(λ₊-1)-(1-p)/(λ₋+1));
 	X=Matrix(simulate(BrownianMotion(σ,drift_RN),spotData,mcBaseData,T,parallelMode))
 
 	t=range(0.0, stop=T, length=Nstep+1);
@@ -69,7 +69,7 @@ function simulate(mcProcess::KouProcess,spotData::equitySpotData,mcBaseData::Mon
 			
 			idx1=findfirst(x->x>=tjump,t);
 			u=rand(); #simulate Uniform([0,1])
-			jump_size=u<p ? quantile_exp(λp,rand()) : -quantile_exp(λm,rand())
+			jump_size=u<p ? quantile_exp(λ₊,rand()) : -quantile_exp(λ₋,rand())
 			X[ii,idx1:end].+=jump_size; #add jump component
 			
 			#for i in 1:Nstep
