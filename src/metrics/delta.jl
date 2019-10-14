09af5d7d7dc7f8ea33638a6d2570ca81af8ab5e7
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-10-14 19:15:04 +0200

    no need set_seed

--- src/metrics/delta.jl
+++ src/metrics/delta.jl
@@ -18,9 +18,7 @@ function delta_macro(model_type)
 		"""	
 		function delta(mcProcess::$model_type,spotData::equitySpotData,mcConfig::MonteCarloConfiguration,abstractPayoff::AbstractPayoff,dS0::Real=1e-7)
 
-			set_seed(mcConfig)
 			Price=pricer(mcProcess,spotData,mcConfig,abstractPayoff);
-			set_seed(mcConfig)
 			spotData_1=equitySpotData(spotData.S0+dS0,spotData.r,spotData.d);
 			PriceUp=pricer(mcProcess,spotData_1,mcConfig,abstractPayoff);
 			Delta=(PriceUp-Price)/dS0;
@@ -34,9 +32,7 @@ delta_macro(BaseProcess)
 
 function delta_macro_array(model_type)
 	@eval function delta(mcProcess::$model_type,spotData::equitySpotData,mcConfig::MonteCarloConfiguration,abstractPayoffs::Array{AbstractPayoff},dS0::Real=1e-7)
-			set_seed(mcConfig)
 			Prices=pricer(mcProcess,spotData,mcConfig,abstractPayoffs);
-			set_seed(mcConfig)
 			spotData_1=equitySpotData(spotData.S0+dS0,spotData.r,spotData.d);
 			PricesUp=pricer(mcProcess,spotData_1,mcConfig,abstractPayoffs);
 			Delta=(PricesUp.-Prices)./dS0;
