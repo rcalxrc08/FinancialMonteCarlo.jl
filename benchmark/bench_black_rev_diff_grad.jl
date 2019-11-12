59876ed8aad15d1ae7980a7de00d27a6a5020d16
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2019-11-12 18:12:04 +0100

    amend

--- benchmark/bench_black_rev_diff_grad.jl
+++ benchmark/bench_black_rev_diff_grad.jl
@@ -23,7 +23,7 @@ AsianFloatingStrikeData=AsianFloatingStrikeOption(T)
 AsianFixedStrikeData=AsianFixedStrikeOption(T,K)
 Model=BlackScholesProcess(sigma,Underlying(S0,d));
 
-f(x) = pricer(BlackScholesProcess(x[1]),ZeroRateCurve(x[2],r,d),mc,EuropeanOption(x[3],K),Underlying(S0,d));
+f(x) = pricer(BlackScholesProcess(x[1],Underlying(x[2],d)),ZeroRateCurve(r),mc,EuropeanOption(x[3],K));
 #f(x) = pricer(BlackScholesProcess(x[1]),ZeroRateCurve(x[2],r,d),mc,EuropeanOption(T,K),Underlying(S0,d));
 #x=Float64[sigma,S0,r,d,T]
 x=Float64[sigma,S0,T]
@@ -33,7 +33,7 @@ y0=g(x);
 @btime g(x);
 
 #f1_(x) = pricer(BlackScholesProcess(x[1]),ZeroRateCurve(x[2],x[3],x[4]),mc,AmericanOption(x[5],K),Underlying(S0,d));
-f1_(x) = pricer(BlackScholesProcess(x[1]),ZeroRateCurve(x[2],r,d),mc,AmericanOption(T,K),Underlying(S0,d));
+f1_(x) = pricer(BlackScholesProcess(x[1],Underlying(x[2],d)),ZeroRateCurve(r),mc,AmericanOption(x[3],K));
 g_ = x -> ReverseDiff.gradient(f1_, x);
 #y0=g(x);
 @btime f1_(x);
