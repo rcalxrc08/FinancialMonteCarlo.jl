5945ffd168f5945d0fc9f3903f4c6d19f950c888
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2020-01-01 22:52:20 +0100

    cv bugged

--- src/metrics/pricer_cv.jl
+++ src/metrics/pricer_cv.jl
@@ -11,7 +11,8 @@ function pricer_macro_2(model_type)
 			S_var=simulate(mcProcess,rfCurve,variate_conf,T)
 			Payoff_var=payoff(S_var,variate_payoff,rfCurve);
 			Payoff_opt_var=payoff(S_var,abstractPayoff,rfCurve);
-			c=-cov(Payoff_var,Payoff_opt_var)/var(Payoff_var);
+			#c=-cov(Payoff_var,Payoff_opt_var)/var(Payoff_var)[1];
+			c=-collect(cov(Payoff_var,Payoff_opt_var)/var(Payoff_var))[1];
 			price_var=mean(Payoff_var);
 			mcConfig_mod=MonteCarloConfiguration(mcConfig.Nsim,mcConfig.Nstep,variate_conf.monteCarloMethod,mcConfig.parallelMode,mcConfig.seed*2)
 			#END OF VARIATE SECTION
@@ -39,16 +40,12 @@ function pricer_macro_3(model_type)
 			S_var=simulate(mcProcess,rfCurve,variate_conf,T)
 			Payoff_var=payoff(S_var,variate_payoff,rfCurve);
 			Payoff_opt_var=payoff(S_var,abstractPayoff,rfCurve);
-			c=cov(Payoff_var,Payoff_opt_var)/var(Payoff_var);
-			@show mean(Payoff_var)
-			@show c
+			c=collect(cov(Payoff_var,Payoff_opt_var)/var(Payoff_var))[1];
 			price_var=mcProcess.underlying.S0;
 			mcConfig_mod=MonteCarloConfiguration(mcConfig.Nsim,mcConfig.Nstep,variate_conf.monteCarloMethod,mcConfig.parallelMode,mcConfig.seed+3)
 			#END OF VARIATE SECTION
 			Prices=pricer(mcProcess,rfCurve,mcConfig_mod,[abstractPayoff,variate_payoff]);
-			@show Prices[2]
 			Price=Prices[1]-c*(Prices[2]-price_var);
-			#@show Prices[1]+c*(Prices[2]-price_var);
 			return Price;
 		end
 	end
