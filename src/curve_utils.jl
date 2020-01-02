1ecaf45b34db426af885a39b0ff76a621b623608
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2020-01-02 18:18:23 +0100

    refactor

--- src/curve_utils.jl
+++ src/curve_utils.jl
@@ -1,45 +1,42 @@
-function oper(+,adict)
+function oper(+)
 	@eval import Base.$+
-	@eval function $+(r::$adict{num1,num2},d::$adict{num1,num2}) where { num1 <: Number, num2 <: Number }
+	@eval function $+(r::FinMCDict{num1,num2},d::FinMCDict{num1,num2}) where { num1 <: Number, num2 <: Number }
 		T_r=collect(keys_(r));
 		T_d=collect(keys_(d));
 		if(length(T_d)>length(T_r))
 			return $+(d,r)
 		end
 		d_complete=complete_(T_r,d);
-		out=$adict{num1,num2}();
+		out=FinMCDict{num1,num2}();
 		for t in T_r
 			insert!(out,t,  $+(r[t], d_complete[t]))
 		end
 
 		return out
 	end
-	@eval function $+(r::$adict{num1,num2},d::num3) where { num1 <: Number, num2 <: Number , num3 <: Number }
+	@eval function $+(r::FinMCDict{num1,num2},d::num3) where { num1 <: Number, num2 <: Number , num3 <: Number }
 		return $+.(r,d)
 	end
 end
-function oper2(+,adict)
+function oper2(+)
 	@eval import Base.$+
-	@eval function $+(d::num3,r::$adict{num1,num2}) where { num1 <: Number, num2 <: Number , num3 <: Number }
+	@eval function $+(d::num3,r::FinMCDict{num1,num2}) where { num1 <: Number, num2 <: Number , num3 <: Number }
 		return $+(r,d)
 	end
 end
 
 import Base.-
-function -(d::num3,r::Dictionary{num1,num2}) where { num1 <: Number, num2 <: Number , num3 <: Number }
-	return +(-1*r,d)
-end
-function -(d::num3,r::HashDictionary{num1,num2}) where { num1 <: Number, num2 <: Number , num3 <: Number }
+function -(d::num3,r::FinMCDict{num1,num2}) where { num1 <: Number, num2 <: Number , num3 <: Number }
 	return +(-1*r,d)
 end
 
-oper(Symbol(+),FinMCDict)
-oper(Symbol(-),FinMCDict)
-oper(Symbol(/),FinMCDict)
-oper(Symbol(*),FinMCDict)
+oper(Symbol(+))
+oper(Symbol(-))
+oper(Symbol(/))
+oper(Symbol(*))
 
-oper2(Symbol(*),FinMCDict)
-oper2(Symbol(+),FinMCDict)
+oper2(Symbol(*))
+oper2(Symbol(+))
 
 
 keys_(x::Dictionary)=x.indices		  
