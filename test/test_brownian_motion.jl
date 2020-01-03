831d176c983bffb1fac08b155e52513b063e042e
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2020-01-03 16:35:46 +0100

    update

--- test/test_brownian_motion.jl
+++ test/test_brownian_motion.jl
@@ -8,6 +8,7 @@ d=0.01;
 T=-Tneg;
 Nsim=10000;
 Nstep=30;
+mc1=MonteCarloConfiguration(Nsim,Nstep,FinancialMonteCarlo.AntitheticMC());
 rfCurve2=FinancialMonteCarlo.ZeroRateCurve2([0.00,0.02],T);
 sigma=0.2; 
 
@@ -20,3 +21,5 @@ drift=0.0
 @test_throws(ErrorException,BrownianMotion(-sigma,rfCurve2.r,Underlying(S0,d)))
 @test_throws(ErrorException,simulate(BrownianMotion(sigma,drift,Underlying(S0,d)),McConfig,Tneg));
 @test_throws(ErrorException,simulate(BrownianMotion(sigma,rfCurve2.r,Underlying(S0,d)),McConfig,Tneg));
+@test_throws(ErrorException,simulate(BrownianMotion(sigma,drift,Underlying(S0,d)),mc1,Tneg));
+@test_throws(ErrorException,simulate(BrownianMotion(sigma,rfCurve2.r,Underlying(S0,d)),mc1,Tneg));
