2d503f7d4b65c39aa2c2a567886aa1101a50d2a5
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2020-01-12 00:37:25 +0100

    bench barrier

--- src/payoffs/general_path_dependent_engine.jl
+++ src/payoffs/general_path_dependent_engine.jl
@@ -7,8 +7,7 @@ function payoff(S::AbstractMatrix{num},payoff_::PathDependentPayoff,rfCurve::abs
 	index1=round(Int,T/T1 * NStep)+1;
 	
 	@inbounds f(S::abstractArray) where {abstractArray<:AbstractArray{num_}} where {num_<:Number}=payout(S,payoff_);
-	#@inbounds payoff2=[f(view(S,i,1:index1)) for i in 1:Nsim];
-	@inbounds payoff2=mapslices(f,view(S,:,1:index1),dims=2);
+	@inbounds payoff2=[f(view(S,i,1:index1)) for i in 1:Nsim];
 	
 	return payoff2*exp(-integral(r,T));
 end
