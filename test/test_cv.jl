69e9f6dc8f34974bc07994211836089cb2b9f743
Nicola Scaramuzzino <nicola.scaramuzzino@yandex.com>
2020-01-11 22:46:24 +0100

    control variate

--- test/test_cv.jl
+++ test/test_cv.jl
@@ -11,7 +11,9 @@ Nsim=10000;
 Nstep=300;
 sigma=0.2;
 variate_=FinancialMonteCarlo.ControlVariates(Forward(T),MonteCarloConfiguration(1000,100))
+variate_2=FinancialMonteCarlo.ControlVariates(AsianFloatingStrikeOption(T),MonteCarloConfiguration(1000,100))
 mc=MonteCarloConfiguration(Nsim,Nstep,variate_);
+mc2=MonteCarloConfiguration(Nsim,Nstep,variate_);
 mc1=MonteCarloConfiguration(Nsim,Nstep);
 toll=1e-3;
 
@@ -27,3 +29,4 @@ Model=BlackScholesProcess(sigma,Underlying(S0,d));
 
 AsianPrice1=pricer(Model,spotData1,mc,AsianFloatingStrikeData);
 AsianPrice2=pricer(Model,spotData1,mc,AsianFixedStrikeData);
+AsianPrice2=pricer(Model,spotData1,mc2,AsianFixedStrikeData);
