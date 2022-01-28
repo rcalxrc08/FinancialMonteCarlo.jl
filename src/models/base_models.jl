943e01297dc874e1c2b39128128c2ec0073f29fa
Nicola <nicola.scaramuzzino@yandex.com>
2022-01-28 11:17:20 +0100

    tests

--- src/models/base_models.jl
+++ src/models/base_models.jl
@@ -82,8 +82,7 @@ Where:
 """
 function simulate(mcProcess::AbstractMonteCarloEngine, mcBaseData::AbstractMonteCarloConfiguration, T::Number)
     price_type = predict_output_type_zero(mcProcess, mcBaseData, T)
-    matrix_type = get_matrix_type(mcBaseData, mcProcess, price_type)
-    S = matrix_type(undef, mcBaseData.Nsim, mcBaseData.Nstep + 1)
+    S = get_matrix_type(mcBaseData, mcProcess, price_type)
     simulate!(S, mcProcess, mcBaseData, T)
     return S
 end
