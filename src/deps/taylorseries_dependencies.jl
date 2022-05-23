492c3d5cfa1bac504ec32818e98de6b7dd72fca9
Nicola <nicola.scaramuzzino@yandex.com>
2022-05-24 00:27:15 +0200

    support for mixed derivatives

--- src/deps/taylorseries_dependencies.jl
+++ src/deps/taylorseries_dependencies.jl
@@ -1,12 +1,15 @@
 using .TaylorSeries
 import Base.max,Base.min,Base.isless
-max(x::AbstractSeries,t::Number)=ifelse(x[0]>t,x,t)
-min(x::AbstractSeries,t::Number)=ifelse(x[0]<t,x,t)
+
+value_taylor(x::Taylor1)=@inbounds @views x[0];
+value_taylor(x::AbstractSeries)=@inbounds @views x[0][1];
+max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)>t,x,t)
+min(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)<t,x,t)
 max(t::Number,x::AbstractSeries)=max(x,t)
 min(t::Number,x::AbstractSeries)=min(x,t)
-max(x::AbstractSeries,t::AbstractSeries)=ifelse(x[0]>t[0],x,t)
-min(x::AbstractSeries,t::AbstractSeries)=ifelse(x[0]<t[0],x,t)
+max(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)>value_taylor(t),x,t)
+min(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)<value_taylor(t),x,t)
 
-isless(t::Number, x::AbstractSeries)=isless(t,x[0])
-isless(x::AbstractSeries,t::Number)=isless(x[0],t)
-isless(x::AbstractSeries,t::AbstractSeries)=isless(x[0],t[0])
+isless(t::Number, x::AbstractSeries)=isless(t,value_taylor(x))
+isless(x::AbstractSeries,t::Number)=isless(value_taylor(x),t)
+isless(x::AbstractSeries,t::AbstractSeries)=isless(value_taylor(x),value_taylor(t))
