e9e50325e9076fe08938853aa4efb8a22fb524ee
Nicola <nicola.scaramuzzino@yandex.com>
2022-05-24 15:01:13 +0200

    optional methods

--- src/deps/taylorseries_dependencies.jl
+++ src/deps/taylorseries_dependencies.jl
@@ -3,13 +3,12 @@ import Base.max,Base.min,Base.isless
 
 value_taylor(x::Taylor1)=@inbounds @views x[0];
 value_taylor(x::AbstractSeries)=@inbounds @views x[0][1];
-max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)>t,x,t)
-min(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)<t,x,t)
-max(t::Number,x::AbstractSeries)=max(x,t)
-min(t::Number,x::AbstractSeries)=min(x,t)
-max(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)>value_taylor(t),x,t)
-min(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)<value_taylor(t),x,t)
-
-isless(t::Number, x::AbstractSeries)=isless(t,value_taylor(x))
-isless(x::AbstractSeries,t::Number)=isless(value_taylor(x),t)
-isless(x::AbstractSeries,t::AbstractSeries)=isless(value_taylor(x),value_taylor(t))
+!hasmethod(max,(AbstractSeries,Number)) ? (max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)>t,x,t)) : nothing
+!hasmethod(max,(Number,AbstractSeries)) ? (max(t::Number,x::AbstractSeries)=max(x,t)) : nothing
+!hasmethod(max,(AbstractSeries,AbstractSeries)) ? (max(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)>value_taylor(t),x,t)) : nothing
+!hasmethod(min,(AbstractSeries,Number)) ? (max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)<t,x,t)) : nothing
+!hasmethod(min,(Number,AbstractSeries)) ? (max(t::Number,x::AbstractSeries)=min(x,t)) : nothing
+!hasmethod(min,(AbstractSeries,AbstractSeries)) ? (max(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)<value_taylor(t),x,t)) : nothing
+!hasmethod(isless,(AbstractSeries,Number)) ? (isless(x::AbstractSeries,t::Number)=isless(value_taylor(x),t)) : nothing
+!hasmethod(isless,(Number,AbstractSeries)) ? (isless(t::Number, x::AbstractSeries)=isless(t,value_taylor(x))) : nothing
+!hasmethod(isless,(AbstractSeries,AbstractSeries)) ? (isless(x::AbstractSeries,t::AbstractSeries)=isless(value_taylor(x),value_taylor(t))) : nothing
