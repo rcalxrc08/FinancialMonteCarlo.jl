c1e3eb1eb47ecb8396966ad2c5bc2cd974851fc2
Nicola <nicola.scaramuzzino@yandex.com>
2022-05-25 12:58:15 +0200

    amend

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,6 @@
 using Documenter, FinancialMonteCarlo, Literate
-getting_started = joinpath(@__DIR__, "..", "examples", "getting_started.jl")
-intdiffeq = joinpath(@__DIR__, "..", "examples", "intdiffeq.jl")
+EXAMPLE = joinpath(@__DIR__, "..", "examples", "getting_started.jl")
 OUTPUT = joinpath(@__DIR__, "src")
-Literate.markdown(getting_started, OUTPUT; documenter = true)
-Literate.markdown(intdiffeq, OUTPUT; documenter = true)
+Literate.markdown(EXAMPLE, OUTPUT; documenter = true)
 makedocs(format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", assets = ["assets/favicon.ico"]), sitename = "FinancialMonteCarlo.jl", modules = [FinancialMonteCarlo], pages = ["index.md", "getting_started.md", "types.md", "stochproc.md", "parallel_vr.md", "payoffs.md", "metrics.md", "multivariate.md", "intdiffeq.md", "extends.md"])
 get(ENV, "CI", nothing) == "true" ? deploydocs(repo = "https://gitlab.com/rcalxrc08/FinancialMonteCarlo.jl.git") : nothing
