312615728922bb89f2eb60838f520638476a5ed4
Nicola <nicola.scaramuzzino@yandex.com>
2022-05-27 14:59:38 +0200

    update deps

--- src/deps/taylorseries_dependencies.jl
+++ src/deps/taylorseries_dependencies.jl
@@ -3,10 +3,10 @@ import Base.max,Base.min,Base.isless
 
 value_taylor(x::Taylor1)=@inbounds @views x[0];
 value_taylor(x::AbstractSeries)=@inbounds @views x[0][1];
-!hasmethod(max,(AbstractSeries,Number)) ? (max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)>t,x,t)) : nothing
+!hasmethod(max,(AbstractSeries,Number)) ? (max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)>=t,x,t)) : nothing
 !hasmethod(max,(Number,AbstractSeries)) ? (max(t::Number,x::AbstractSeries)=max(x,t)) : nothing
 !hasmethod(max,(AbstractSeries,AbstractSeries)) ? (max(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)>value_taylor(t),x,t)) : nothing
-!hasmethod(min,(AbstractSeries,Number)) ? (max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)<t,x,t)) : nothing
+!hasmethod(min,(AbstractSeries,Number)) ? (max(x::AbstractSeries,t::Number)=ifelse(value_taylor(x)<=t,x,t)) : nothing
 !hasmethod(min,(Number,AbstractSeries)) ? (max(t::Number,x::AbstractSeries)=min(x,t)) : nothing
 !hasmethod(min,(AbstractSeries,AbstractSeries)) ? (max(x::AbstractSeries,t::AbstractSeries)=ifelse(value_taylor(x)<value_taylor(t),x,t)) : nothing
 !hasmethod(isless,(AbstractSeries,Number)) ? (isless(x::AbstractSeries,t::Number)=isless(value_taylor(x),t)) : nothing
