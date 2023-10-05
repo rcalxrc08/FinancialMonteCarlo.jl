ccb0ee57056bd968d08919c1f10389c7b6b01cce
Nicola <nicola.scaramuzzino@yandex.com>
2023-10-05 13:03:35 +0200

    update benchmark

--- benchmark/bench_black.jl
+++ benchmark/bench_black.jl
@@ -11,18 +11,18 @@ D = 90.0;
 Nsim = 10000;
 Nstep = 30;
 sigma = 0.2;
-mc = MonteCarloConfiguration(Nsim, Nstep);
+const mc = MonteCarloConfiguration(Nsim, Nstep);
 toll = 1e-3;
 
-rfCurve = ZeroRate(r);
+const rfCurve = ZeroRate(r);
 
-FwdData = Forward(T)
-EUData = EuropeanOption(T, K)
-AMData = AmericanOption(T, K)
-BarrierData = BarrierOptionDownOut(T, K, D)
-AsianFloatingStrikeData = AsianFloatingStrikeOption(T)
-AsianFixedStrikeData = AsianFixedStrikeOption(T, K)
-Model = BlackScholesProcess(sigma, Underlying(S0, d));
+const FwdData = Forward(T)
+const EUData = EuropeanOption(T, K)
+const AMData = AmericanOption(T, K)
+const BarrierData = BarrierOptionDownOut(T, K, D)
+const AsianFloatingStrikeData = AsianFloatingStrikeOption(T)
+const AsianFixedStrikeData = AsianFixedStrikeOption(T, K)
+const Model = BlackScholesProcess(sigma, Underlying(S0, d));
 
 @btime FwdPrice = pricer(Model, rfCurve, mc, FwdData);
 @btime EuPrice = pricer(Model, rfCurve, mc, EUData);
@@ -31,6 +31,6 @@ Model = BlackScholesProcess(sigma, Underlying(S0, d));
 @btime AsianPrice1 = pricer(Model, rfCurve, mc, AsianFloatingStrikeData);
 @btime AsianPrice1 = pricer(Model, rfCurve, mc, AsianFixedStrikeData);
 
-optionDatas = [FwdData, EUData, AMData, BarrierData, AsianFloatingStrikeData, AsianFixedStrikeData]
+const optionDatas = [FwdData, EUData, AMData, BarrierData, AsianFloatingStrikeData, AsianFixedStrikeData]
 
 @btime (FwdPrice, EuPrice, AMPrice, BarrierPrice, AsianPrice1, AsianPrice2) = pricer(Model, rfCurve, mc, optionDatas)
