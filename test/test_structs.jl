d65b14da3522253bc2b366b158d7bb0b52a56a73
Nicola <nicola.scaramuzzino@yandex.com>
2026-01-24 13:31:20 +0100

    add smiles tests

--- test/test_structs.jl
+++ test/test_structs.jl
@@ -90,3 +90,23 @@ struct tmptype2 <: FinancialMonteCarlo.AbstractPayoff{Float64}
 end
 
 @test_throws(ErrorException, payoff([1.0 1.0; 1.0 1.0], tmptype2(1.0), rfCurve, mc1));
+
+using FinancialMonteCarlo
+
+@testset "Smiles Struct Consistency" begin
+    K_vec = [90.0, 100.0, 110.0]
+    K_vec_w = [90.0, 100.0, 110.0, 111.0]
+    K_vec_w2 = [90.0, 100.0, -111.0]
+    isCall_vec = [true, false, true]
+    T = 1.0
+    smile_eu = FinancialMonteCarlo.EuropeanOptionSmile(T, K_vec, isCall_vec)
+    smile_bin = FinancialMonteCarlo.BinaryEuropeanOptionSmile(T, K_vec, isCall_vec)
+
+    @test_throws "K and isCall must have the same length" FinancialMonteCarlo.EuropeanOptionSmile(T, K_vec_w, isCall_vec)
+    @test_throws "Strikes must be positive" FinancialMonteCarlo.EuropeanOptionSmile(T, K_vec_w2, isCall_vec)
+    @test_throws "Maturity must be positive" FinancialMonteCarlo.EuropeanOptionSmile(-1.0, K_vec, isCall_vec)
+
+    @test_throws "K and isCall must have the same length" FinancialMonteCarlo.BinaryEuropeanOptionSmile(T, K_vec_w, isCall_vec)
+    @test_throws "Strikes must be positive" FinancialMonteCarlo.BinaryEuropeanOptionSmile(T, K_vec_w2, isCall_vec)
+    @test_throws "Maturity must be positive" FinancialMonteCarlo.BinaryEuropeanOptionSmile(-1.0, K_vec, isCall_vec)
+end
